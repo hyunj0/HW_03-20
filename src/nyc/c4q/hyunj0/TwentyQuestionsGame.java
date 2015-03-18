78be5bc12943f9db0b9418b9ec5f21e4a5524508
hyunj0 <hyunj0@hotmail.com>
2015-03-18 00:17:24 -0400

    Updated Twenty Questions Game

--- src/nyc/c4q/hyunj0/TwentyQuestionsGame.java
+++ src/nyc/c4q/hyunj0/TwentyQuestionsGame.java
@@ -1,32 +1,47 @@
 package nyc.c4q.hyunj0;
 
-/**
- * Created by c4q-hyunj0 & alizinha on 3/16/15.
- */
+//Created by c4q-hyunj0 & alizinha on 3/16/15.
+//Updated by c4q-hyunj0 on 3/17/15.
 
 import java.util.Scanner;
 import java.util.Random;
 
 public class TwentyQuestionsGame {
     public static void main (String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+
+        print("Play the 20 questions game. Guess the number between 1 to 100,000!");
+
+        //generate a random number from 1 to 100,000
+        //+1 is there to include the 100,000 since the range would be 1 to 99,999 otherwise (zero-indexed)
         int number = new Random().nextInt(100000) + 1;
 
-        while (true) {
-            System.out.println("guess? ");
-            int guess = scanner.nextInt();
+        //count how many guesses it takes to guess correctly
+        int counter = 0;
 
-            if (guess < 1 || guess > 100000)
-                System.out.println("please guess between 1 and 100,000");
-            else if (guess > number)
-                System.out.println("too high!");
-            else if (guess < number)
-                System.out.println("too low!");
-            else {
-                System.out.println("that's right!");
-                break;
+        //make a for loop ending at 20 tries
+        for (int i = 0; i < 20; i++) {
+            System.out.print("Your guess: ");
+            int guess = input.nextInt();
+            if (guess == number) {
+                counter += 1;
+                print("You guessed correct in " + counter + " tries. Good job!!");
+            } else if (guess > number) {
+                print("You guessed too high.");
+                counter += 1;
+            } else if (guess < number) {
+                print("You guessed too low.");
+                counter += 1;
+            } else {
+                print("Guess a number from 1 to 100,000 only!");
+                counter += 1;
             }
         }
+
+        print("Sorry, you ran out of guesses. You lost :(");
     }
 
+    public static void print (String string) {
+        System.out.println(string);
+    }
 }
